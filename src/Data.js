7a7ab1d1582c49c3b6352b58782a156781759361
w3sl1y <SV73623892@idat.edu.pe>
2021-10-22 19:55:51 -0500

    first commit

---
+++ src/Data.js
@@ -0,0 +1,39 @@
+const data = {
+    status: "success",
+    items: [
+      {
+        id: 1,
+        title: "Aston",
+        price: 401,
+        image: require('./images/img01.jpg'),
+        category: "wanelo",
+        cantidad: 1,
+      },
+      {
+        id: 2,
+        title: "Lady at twentytwentyone",
+        price: 504,
+        image: require('./images/img02.jpg'),
+        category: "wanelo",
+        cantidad: 1
+      },
+      {
+        id: 3,
+        title: "Fotel Empoli styl glamour złoty pepitka",
+        price: 677,
+        image: require('./images/img03.jpg'),
+        category: "wanelo",
+        cantidad: 1
+      },
+      {
+        id: 4,
+        title: "Oxford",
+        price: 499,
+        image: require('./images/img04.jpg'),
+        category: "wanelo",
+        cantidad: 1
+      },
+    ],
+  };
+  
+  export default data;
